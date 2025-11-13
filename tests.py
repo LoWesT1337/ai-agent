8844a6aed2e971790755abb942e590a3ca5a79e5
LoWesT13 <killroad13@hotmail.com>
2025-11-13 21:56:41 +0100

    updating ai-agent stuff

---
+++ tests.py
@@ -0,0 +1,14 @@
+from calculator.pkg.calculator import Calculator
+import unittest
+from  functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
+from functions.write_file import write_file
+from functions.run_python_file import run_python_file
+
+
+#print(run_python_file("calculator", "main.py")) #(should print the calculator's usage instructions)
+#print(run_python_file("calculator", "main.py", ["3 + 5"])) #(should run the calculator... which gives a kinda nasty rendered result)
+#print(run_python_file("calculator", "tests.py"))
+#print(run_python_file("calculator", "../main.py")) #(this should return an error)
+#print(run_python_file("calculator", "nonexistent.py")) #(this should return an error)
+#print(run_python_file("calculator", "lorem.txt"))
